bcfe7fd3473620d7b361536ea5f781fc5b078036
Naw Kanyaw <naha72478@gmail.com>
2025-12-18 08:39:00 +0630

    template1

---
+++ FrontEnd/templates/template1/index.php
@@ -0,0 +1,31 @@
+<?php
+include("partial/header.php");
+?>
+
+<div class="content">
+    <img class="homeimg" src="" alt="">
+    <div class="cardslot">
+        <div class="card">
+            <img class="cardimg" src="" alt="">
+            <h1 class="cardtitle">Chinese Food</h1>
+            <a href="categoryView.php?category=chinese" class="">View</a>
+        </div>
+        <div class="card">
+            <img class="cardimg" src="" alt="">
+            <h1 class="cardtitle">Thai Food</h1>
+            <a href="categoryView.php?category=thai" class="">View</a>
+        </div>
+        <div class="card">
+            <img class="cardimg" src="" alt="">
+            <h1 class="cardtitle">Myanmar Food</h1>
+            <a href="categoryView.php?category=myanmar" class="">View</a>
+        </div>
+    </div>
+
+</div>
+
+<?php
+include("partial/footer.php");
+?>
+
+<!-- run the file that is inside the include parameter as soon as it's launch -->
