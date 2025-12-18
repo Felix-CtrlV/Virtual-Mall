1eac7c13b35beae44286aa5e5525d8d2f434f850
Naw Kanyaw <naha72478@gmail.com>
2025-12-18 09:28:50 +0630

    ff

--- FrontEnd/templates/template1/index.php
+++ FrontEnd/templates/template1/index.php
@@ -7,17 +7,17 @@ include("partial/header.php");
     <div class="cardslot">
         <div class="card">
             <img class="cardimg" src="" alt="">
-            <h1 class="cardtitle">Chinese Food</h1>
+            <h1 class="cardtitle"></h1>
             <a href="categoryView.php?category=chinese" class="">View</a>
         </div>
         <div class="card">
             <img class="cardimg" src="" alt="">
-            <h1 class="cardtitle">Thai Food</h1>
+            <h1 class="cardtitle"></h1>
             <a href="categoryView.php?category=thai" class="">View</a>
         </div>
         <div class="card">
             <img class="cardimg" src="" alt="">
-            <h1 class="cardtitle">Myanmar Food</h1>
+            <h1 class="cardtitle"></h1>
             <a href="categoryView.php?category=myanmar" class="">View</a>
         </div>
     </div>
